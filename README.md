e813727c66b8ac879a3df3317e72352b4e11a1d1
Laetitia Constant <laetitiaconstant@MacBook-Pro-de-Laetitia.local>
2021-05-17 18:08:58 +0200

    clean code and add comments on readme page

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# yuka
+<h1 align="center">Yuka - Clone</h1>
+
+## Overview
+
+This project uses [OpenFoodFacts](https://fr.openfoodfacts.org/) API and allows to :
+
+- Scan a product ( possibility to set the torch mode )
+- View the nutritional value of the scanned product
+- Visualise the scan history
+- Add a scanned product to favorites
+
+&nbsp;
+
+## Screenshots of application
+
+<img src="./assets/screen1.png" alt="drawing" width="190"/>
+
+<img src="./assets/screen3.png" alt="drawing" width="190"/>
+
+<img src="./assets/screen2.png" alt="drawing" width="190"/>
+
+&nbsp;
+
+## Running the project
+
+First make sure you have installed all dependencies and applications to run project on your computer : [Getting Started with Expo](https://docs.expo.io/get-started/installation/).
+
+This project works fine for iOS but not on Android device because I've only worked on iOS.
+
+Clone this repository :
+
+```
+git clone https://github.com/LaetitiaConstant/yuka.git
+cd yuka
+```
+
+Install packages :
+
+```
+yarn
+```
+
+When installation is complete, run the application :
+
+```
+expo start
+```
+
+You can download **Expo Go** on your mobile and scan code or open **iOS simulator**.
--- containers/HistoriqueScreen.js
+++ containers/HistoriqueScreen.js
@@ -1,16 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { useNavigation } from "@react-navigation/core";
 import {
-  Button,
-  StatusBar,
-  ScrollView,
-  Text,
-  StyleSheet,
-  TouchableOpacity,
-  ActivityIndicator,
-  Image,
-  SafeAreaView,
-  View,
+	StatusBar,
+	ScrollView,
+	Text,
+	StyleSheet,
+	TouchableOpacity,
+	ActivityIndicator,
+	Image,
+	SafeAreaView,
+	View,
 } from "react-native";
 import * as HistoriqueManager from "../components/HistoriqueManager";
 import axios from "axios";
@@ -19,119 +18,111 @@ import Product from "../components/Product";
 import Constants from "expo-constants";
 
 export default function HistoriqueScreen() {
-  const navigation = useNavigation();
-  const [isLoading, setIsLoading] = useState(true);
-  const [products, setProducts] = useState([]);
+	const navigation = useNavigation();
+	const [isLoading, setIsLoading] = useState(true);
+	const [products, setProducts] = useState([]);
 
-  // Delete Item
-  const deleteItem = async (code) => {
-    return await HistoriqueManager.DeleteData(code);
-  };
+	// Delete Item
+	const deleteItem = async (code) => {
+		return await HistoriqueManager.DeleteData(code);
+	};
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const datas = await HistoriqueManager.Load();
-        setProducts(
-          await Promise.all(
-            datas.map(async (data) => {
-              const response = await axios.get(
-                `https://world.openfoodfacts.org/api/v0/product/${data}.json`
-              );
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const datas = await HistoriqueManager.Load();
+				setProducts(
+					await Promise.all(
+						datas.map(async (data) => {
+							const response = await axios.get(
+								`https://world.openfoodfacts.org/api/v0/product/${data}.json`
+							);
 
-              return response.data.product;
-            })
-          )
-        );
-        setIsLoading(false);
-      } catch (error) {
-        console.log(error);
-      }
-    };
+							return response.data.product;
+						})
+					)
+				);
+				setIsLoading(false);
+			} catch (error) {
+				console.log(error);
+			}
+		};
 
-    fetchData();
-  });
+		fetchData();
+	});
 
-  return isLoading ? (
-    <SafeAreaView style={styles.loadingPage}>
-      <Image style={styles.loading} source={require("../assets/loading.png")} />
-      <ActivityIndicator size="large" color="grey" />
-    </SafeAreaView>
-  ) : (
-    <SafeAreaView style={styles.bgc}>
-      <ScrollView
-        showsVerticalScrollIndicator={false}
-        style={styles.scrollView}
-      >
-        <StatusBar barStyle="dark-content" />
-        <Text style={styles.title}>Historique</Text>
-        {products.map((product, index) => {
-          return (
-            <View key={index}>
-              <TouchableOpacity
-                data={product}
-                onPress={() => {
-                  deleteItem(product.code);
-                }}
-              >
-                <AntDesign
-                  style={styles.close}
-                  name="close"
-                  size={18}
-                  color="black"
-                />
-              </TouchableOpacity>
+	return isLoading ? (
+		<SafeAreaView style={styles.loadingPage}>
+			<Image style={styles.loading} source={require("../assets/loading.png")} />
+			<ActivityIndicator size="large" color="grey" />
+		</SafeAreaView>
+	) : (
+		<SafeAreaView style={styles.bgc}>
+			<ScrollView showsVerticalScrollIndicator={false} style={styles.scrollView}>
+				<StatusBar barStyle="dark-content" />
+				<Text style={styles.title}>Historique</Text>
+				{products.map((product, index) => {
+					return (
+						<View key={index}>
+							<TouchableOpacity
+								data={product}
+								onPress={() => {
+									deleteItem(product.code);
+								}}
+							>
+								<AntDesign style={styles.close} name="close" size={18} color="black" />
+							</TouchableOpacity>
 
-              <TouchableOpacity
-                onPress={() => {
-                  navigation.navigate("ProductScreen", product);
-                }}
-              >
-                <Product product={product} />
-              </TouchableOpacity>
-              <View style={styles.line}></View>
-            </View>
-          );
-        })}
-      </ScrollView>
-    </SafeAreaView>
-  );
+							<TouchableOpacity
+								onPress={() => {
+									navigation.navigate("ProductScreen", product);
+								}}
+							>
+								<Product product={product} />
+							</TouchableOpacity>
+							<View style={styles.line}></View>
+						</View>
+					);
+				})}
+			</ScrollView>
+		</SafeAreaView>
+	);
 }
 
 const styles = StyleSheet.create({
-  loading: {
-    height: 200,
-    width: 200,
-    resizeMode: "contain",
-  },
-  loadingPage: {
-    justifyContent: "center",
-    alignItems: "center",
-    height: "100%",
-    backgroundColor: "#209952",
-    height: "100%",
-  },
-  scrollView: {
-    marginTop: Platform.OS === "android" ? Constants.statusBarHeight : 0,
-  },
-  bgc: {
-    backgroundColor: "white",
-  },
-  line: {
-    height: 10,
-    width: "90%",
-    marginLeft: "10%",
-    borderColor: "#F1F1F2",
-    borderBottomWidth: 1,
-  },
-  close: {
-    marginLeft: 10,
-  },
-  title: {
-    fontWeight: "bold",
-    fontSize: 40,
-    marginBottom: 20,
-    marginTop: 20,
-    marginLeft: 10,
-  },
+	loading: {
+		height: 200,
+		width: 200,
+		resizeMode: "contain",
+	},
+	loadingPage: {
+		justifyContent: "center",
+		alignItems: "center",
+		height: "100%",
+		backgroundColor: "#209952",
+		height: "100%",
+	},
+	scrollView: {
+		marginTop: Platform.OS === "android" ? Constants.statusBarHeight : 0,
+	},
+	bgc: {
+		backgroundColor: "white",
+	},
+	line: {
+		height: 10,
+		width: "90%",
+		marginLeft: "10%",
+		borderColor: "#F1F1F2",
+		borderBottomWidth: 1,
+	},
+	close: {
+		marginLeft: 10,
+	},
+	title: {
+		fontWeight: "bold",
+		fontSize: 40,
+		marginBottom: 20,
+		marginTop: 20,
+		marginLeft: 10,
+	},
 });
